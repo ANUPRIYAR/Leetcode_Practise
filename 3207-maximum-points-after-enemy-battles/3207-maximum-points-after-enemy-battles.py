31d2f13d1644ae68404a1a87d997febe67bad5cf
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-07-16 18:29:23 +0530

    Time: 571 ms (13.34%), Space: 31.8 MB (80.47%) - LeetHub

--- 3207-maximum-points-after-enemy-battles/3207-maximum-points-after-enemy-battles.py
+++ 3207-maximum-points-after-enemy-battles/3207-maximum-points-after-enemy-battles.py
@@ -2,8 +2,8 @@ class Solution:
     def maximumPoints(self, enemyEnergies: List[int], currentEnergy: int) -> int:
         enemyEnergies.sort()
         start, end = 0, len(enemyEnergies) - 1
-        # points = 0
-        print(enemyEnergies[start])
+
+
         points = currentEnergy// enemyEnergies[start]
         currentEnergy = currentEnergy % enemyEnergies[start]
         

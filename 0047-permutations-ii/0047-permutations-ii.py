8e34719fd80640b2130aca2f2acfc601407c8ee0
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-07-18 13:08:51 +0530

    Time: 683 ms (5%), Space: 16.8 MB (37.99%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def permute_backtrack(current, freq, result):
+            if len(current) == len(nums):
+                if current not in result:
+                    result.append(current[:])
+                return 
+
+
+            for i in range(len(nums)):
+                # if i > 0 and nums[i]==nums[i-1]:
+                #     continue
+                if freq[i] == 0:
+                    current.append(nums[i])
+                    freq[i] = 1
+                    permute_backtrack(current, freq, result)
+                    current.pop()
+                    freq[i] = 0
+
+        result = []
+        freq = dict.fromkeys(range(0,len(nums)), 0)
+        permute_backtrack([], freq, result)
+        return result
+
+
+                
+        

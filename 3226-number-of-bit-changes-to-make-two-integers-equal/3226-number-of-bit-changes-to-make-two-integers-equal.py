dd143d2ee594a6563a3c2bcac18e016ce37415c4
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-07-23 12:18:36 +0530

    Time: 38 ms (39.6%), Space: 16.6 MB (46.78%) - LeetHub

--- 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.py
+++ 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.py
@@ -1,7 +1,6 @@
 class Solution:
     def minChanges(self, n: int, k: int) -> int:
-        # check if unset but if n is also unsert for k
-        unset_bits = [1]*32
+        # check if unset but if n is also unset for k
         for i in range(32):
             if n & 1<<i == 0:
                 if k & 1 << i != 0:

e90d49619ac596078ed7f6cc1ca8e73cd3bfb234
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-07-27 22:47:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
@@ -2,9 +2,8 @@ class Solution:
     def minKBitFlips(self, nums: List[int], k: int) -> int:
         cur_window_flips = 0
         res = 0
-        n = len(nums)
 
-        for i in range(n):
+        for i in range(len(nums)):
             # check if cur index is Out of Bounds for k size window
             if i - k >= 0 and nums[i -k] == 2:
                 cur_window_flips -= 1    # then we reduce the effect of atleast 1 window which started at i-k index

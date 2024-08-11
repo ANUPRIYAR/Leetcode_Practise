eb1b6d18ef81928563a736b45433649549003c73
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-08-11 20:00:04 +0530

    Time: 234 ms (5.01%), Space: 16.6 MB (54.04%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
@@ -1,21 +1,23 @@
 class Solution:
     def mctFromLeafValues(self, arr: List[int]) -> int:
-        return self.helper(arr, 0, len(arr) - 1 , {})
+        return self.helper(arr, 0, len(arr) - 1, {})
 
-    def helper(self, arr, l, r , dp):
+    def helper(self, arr, l, r, dp):
         if (l, r) in dp:
             return dp[(l, r)]
 
         if l >= r:
             return 0
 
-        res = float('inf')
+        res = float("inf")
 
         for k in range(l, r):
-            rootVal = max(arr[l:k + 1]) * max(arr[k + 1 : r + 1])
-            res = min(res, rootVal + self.helper(arr, l, k, dp) + self.helper(arr, k + 1, r, dp))
+            rootVal = max(arr[l : k + 1]) * max(arr[k + 1 : r + 1])
+            res = min(
+                res,
+                rootVal + self.helper(arr, l, k, dp) + self.helper(arr, k + 1, r, dp),
+            )
 
         dp[(l, r)] = res
 
         return dp[(l, r)]
-        

4a6f3bf2e99022787151e29b8f12fc3acafc6d9b
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-08-15 20:05:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,35 +1,27 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        bill_counter = dict()
-        bill_counter[5] = bill_counter[10] = bill_counter[20] = 0
+        fives, tens, twenties = 0, 0, 0
+        cost = 5
+        balance = 0
 
-        for  bill in bills:
-            
-            if bill != 5:
-                change = bill - 5
-                if change > 0:
-                    if bill_counter.get(change, 0) > 0:
-                        bill_counter[change] -= 1
-                    elif change == 5:
-                        return False
-                    elif change == 15:
-                        if bill_counter[10] > 0 and bill_counter[5] > 0:
-                            bill_counter[10] -= 1
-                            bill_counter[5] -= 1
-                        elif bill_counter[5] >= 3:
-                            bill_counter[5] -= 3
-                        else:
-                            return False
-            bill_counter[bill] += 1
+        for bill in bills:
+            balance = bill - cost
+            if balance == 15:
+                # if tens > 0:
+                #     tens -= 1
+                # else:
+                #     fives -= 2
+                tens -= 1 if tens > 0 else(fives := fives - 2)
+                fives -= 1 
+            elif balance == 5:
+                fives -= 1
+            if fives < 0:
+                return False
+            if bill == 5:
+                fives += 1
+            if bill == 10:
+                tens += 1
+            if bill == 20:
+                twenties += 1
 
         return True
-
-
-
-
-
-
-                
-
-
-        

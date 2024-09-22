4ae40b5197b2ee9e2ee0968b4de0950e04c65fc7
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-09-22 21:38:15 +0530

    Time: 369 ms (39.19%), Space: 42.6 MB (5.07%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
@@ -1,32 +1,18 @@
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
-        # Step 1: Count the frequency of each element
-        elementFrequency = Counter(arr)
+        hashmap = dict()
 
-        n = len(arr)
+        for num in arr:
+            hashmap[num] = hashmap.get(num, 0) + 1
 
-        # Step 2: Array to count how many elements have a certain frequency
-        frequencyCount = [0] * (n + 1)
+        hashmap = dict(sorted(hashmap.items(), key = lambda x:x [1]))
 
-        # Step 3: Fill the frequencyCount array based on elementFrequency map
-        for freq in elementFrequency.values():
-            frequencyCount[freq] += 1
+        n = len(hashmap)
+        for key in hashmap:
+            if hashmap[key] <= k:
+                k -= hashmap[key]
+                n -= 1
+        return n
+            
 
-        # Total unique elements in the array
-        uniqueElements = len(elementFrequency)
 
-        # Step 4: Iterate through possible frequencies and remove elements
-        for i in range(1, n + 1):
-            # Calculate the number of elements to remove with the current frequency 'i'
-            elementsToRemove = min(k // i, frequencyCount[i])
-
-            # Update 'k' by reducing the count of elements we can remove
-            k -= i * elementsToRemove
-            # Decrease the count of unique elements by the number of elements removed
-            uniqueElements -= elementsToRemove
-
-            # If we no longer have enough 'k' to remove another element with the current frequency, return the number of remaining unique elements
-            if k < i:
-                return uniqueElements
-
-        return 0

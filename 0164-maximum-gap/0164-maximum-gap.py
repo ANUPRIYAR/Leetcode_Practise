5a399f561685479aab136e148399d08fdd1888ee
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-10-03 17:03:00 +0530

    Time: 943 ms (25.28%), Space: 31 MB (30.41%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -1,46 +1,24 @@
+
+import heapq
+
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
-        self.radix_sort(nums)
-        max_gap = 0
         if len(nums) < 2:
             return 0
 
-        for i in range(len(nums) - 1):
-            if nums[i +1] - nums[i] > max_gap:
-                max_gap = nums[i +1] - nums[i]
-
-        return max_gap
-
-
-    def radix_sort(self, nums):
-        max_val = max(nums)
-
-        exp = 1
-        while max_val//exp > 0:
-            self.counting_sort(nums, exp)
-            exp *= 10
-        
-
-    def counting_sort(self, nums, exp):
-        count = [0]* 10
-        output = [0] * len(nums)
-
+        minheap = []
         for num in nums:
-            digit = (num//exp)% 10
-            count[digit] += 1
+            heapq.heappush(minheap, num)
 
-        for i in range(1, len(count)):
-            count[i] += count[i-1]
+        max_diff = float(-inf)
+        while minheap:
+            num1 = heapq.heappop(minheap)
 
-        for i in range(len(nums) - 1, -1, -1):
-            digit = (nums[i]//exp)% 10
-            output[count[digit] - 1] = nums[i]
-            count[digit] -= 1
+            if minheap:
+                max_diff = max(max_diff, (minheap[0] - num1))
 
-        for i in range(len(nums)):
-            nums[i] = output[i]
+        return max_diff
 
 
 
-        
         

bfc605a8fe6916c4e02e073f9a8508c94c8f4496
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-10-03 17:02:21 +0530

    Time: 1452 ms (5.05%), Space: 29.7 MB (97.01%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -1,6 +1,3 @@
-
-import heapq
-
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
         self.radix_sort(nums)
@@ -8,13 +5,11 @@ class Solution:
         if len(nums) < 2:
             return 0
 
-        maxheap = []
         for i in range(len(nums) - 1):
-            diff = nums[i + 1] - nums[i]
-            heapq.heappush(maxheap, - diff)
+            if nums[i +1] - nums[i] > max_gap:
+                max_gap = nums[i +1] - nums[i]
 
-
-        return -heapq.heappop(maxheap)
+        return max_gap
 
 
     def radix_sort(self, nums):
@@ -27,26 +22,21 @@ class Solution:
         
 
     def counting_sort(self, nums, exp):
-        count = [0] * 10
+        count = [0]* 10
         output = [0] * len(nums)
 
-        # count freqeuncy
         for num in nums:
             digit = (num//exp)% 10
             count[digit] += 1
 
-
-        #  cumulative freqeuncy
         for i in range(1, len(count)):
             count[i] += count[i-1]
 
-        # Update output
         for i in range(len(nums) - 1, -1, -1):
             digit = (nums[i]//exp)% 10
             output[count[digit] - 1] = nums[i]
             count[digit] -= 1
 
-        # reset nums
         for i in range(len(nums)):
             nums[i] = output[i]
 

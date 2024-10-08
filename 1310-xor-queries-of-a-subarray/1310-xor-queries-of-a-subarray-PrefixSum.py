0662089da08116edc4b66351634bea0bd83e2998
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-10-08 22:44:55 +0530

    Time: 836 ms (5.27%), Space: 34.4 MB (5.22%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray-PrefixSum.py
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray-PrefixSum.py
@@ -1,21 +1,19 @@
 import math
-class Query:
-    def __init__(self, left, right, index):
-        self.left = left
-        self.right = right
-        self.index = index
 
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
         n = len(arr)
-        q = len(queries)
 
-        prefix_xor = [0]* (n + 1)
-        for i in range(1, n + 1):
-            prefix_xor[i] = prefix_xor[i-1] ^ arr[i-1]
+        prefix_xor = [0]* n
+        prefix_xor[0] = arr[0]
+        for i in range(1, n ):
+            prefix_xor[i] = prefix_xor[i-1] ^ arr[i]
 
         result = []
         for left, right in queries:
-            result.append(prefix_xor[right + 1] ^ prefix_xor[left])
+            if left == 0:
+                result.append(prefix_xor[right])
+            else:
+                result.append(prefix_xor[right] ^ prefix_xor[left - 1])
 
         return result

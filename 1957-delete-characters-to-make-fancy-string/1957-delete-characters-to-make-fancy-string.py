1154d1c0d6e9c64c7e47521a4a6e8896727ef306
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-01 06:15:07 +0530

    Time: 319 ms (54.05%), Space: 18.4 MB (83.28%) - LeetHub

---
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
@@ -0,0 +1,29 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        fancy_string = ""
+        start, end = 0, 1
+        n = len(s)
+
+        if len(s)<= 2:
+            return s
+
+        while end < n:
+            while  end < n and s[start] == s[end]:
+                end += 1
+
+            if end - start >= 3:
+                fancy_string += s[start]*2
+            else:
+                fancy_string += s[start:end]
+            
+            start = end
+            end += 1
+
+        if len(s)> 2 and s[-1] != s[-2]:
+            fancy_string += s[-1]
+        
+        return fancy_string
+
+        
+
+        

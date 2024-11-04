ecd65bccf1403f6ed11ca74c99752931f2bba9f5
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-04 10:45:00 +0530

    Time: 431 ms (19.15%), Space: 28.3 MB (8.56%) - LeetHub

---
+++ 3163-string-compression-iii/3163-string-compression-iii.py
@@ -0,0 +1,29 @@
+from collections import deque
+class Solution:
+    def compressedString(self, word: str) -> str:
+        n = len(word)
+        stack = deque()
+        stack.append([word[0] , 1])
+        comp = ""
+        i = 1
+        while stack and i < n:
+            if stack[-1][0] == word[i]:
+                _, count = stack.pop()
+                count += 1
+                stack.append([word[i], count])       
+            else:
+                stack.append([word[i], 1])
+            i += 1
+            
+        while stack:
+            char, count = stack.popleft()
+            while count > 9:
+                comp += f"9{char}"
+                count -= 9
+            if count > 0:
+                comp += f"{count}{char}"
+        
+        return comp
+            
+        
+       

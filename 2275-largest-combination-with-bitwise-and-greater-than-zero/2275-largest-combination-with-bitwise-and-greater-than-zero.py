d8ae94742eb17841374d819d0480ffd11417c2cc
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-10 17:09:49 +0530

    Time: 879 ms (10.28%), Space: 26.8 MB (55.62%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.py
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.py
@@ -5,7 +5,7 @@ class Solution:
         max_freq = 0
         for num in candidates:
             for i in range(32):
-                x = 1<<i
+                x = 1 << i
                 if num &  x != 0:
                     bit_count[i] += 1
                     if bit_count[i] > max_freq:

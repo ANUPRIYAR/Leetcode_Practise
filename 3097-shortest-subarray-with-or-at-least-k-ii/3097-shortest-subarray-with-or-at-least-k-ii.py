64647d4b86079d19ddc0edf024ca3c07c6acf852
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-10 22:29:28 +0530

    Time: 1221 ms (72.09%), Space: 35.6 MB (87.97%) - LeetHub

--- 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.py
+++ 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.py
@@ -1,22 +1,41 @@
 class Solution:
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
-        res = float('inf')
-        start = window = 0
-        bits = [0] * 32
+        start = 0
+        or_result = 0
+        min_length = float('inf')
+        bit_count = [0] * 32
+
         for end in range(len(nums)):
-            window |= nums[end]
+            or_result |= nums[end]
             for i in range(32):
-                if nums[end] & (1 << i):
-                    bits[i] += 1
+                if nums[end] & 1<<i:
+                    bit_count[i] += 1
+
+            while start <= end and or_result >= k:
+                min_length = min(min_length, end - start + 1)
+                start_num = nums[start]
+                for i in range(32) :
+                    if start_num & 1<<i:
+                        bit_count[i] -= 1
+                        if bit_count[i] == 0:
+                            or_result = 0
+                            for i in range(32):
+                                if bit_count[i] > 0:
+                                    or_result += pow(2, i)
+                start  += 1
+
+        return min_length if min_length != float('inf') else -1 
+
+
+
+
+
+
+
+
+
+
+
+
+
         
-            while start <= end and window >= k:
-                res = min(res, end - start + 1)
-                for i in range(32):
-                    out = nums[start] & (1 << i)
-                    # print(f"{nums[start]} & {1 << i} : {out}")
-                    if out:
-                        bits[i] -= 1
-                        if bits[i] == 0:
-                            window ^= 1 << i
-                start += 1
-        return res if res < float('inf') else -1

f8783fb8eb60a599b37f20330deb93d7e643f6ec
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-11 13:38:35 +0530

    Time: 78 ms (17.98%), Space: 29.9 MB (96.38%) - LeetHub

---
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        q = len(nums)
+        query = [-1]* q
+        prefix_xor, index, max_bitlength = 0, 0, 0
+
+        # Calculating prefix_xor in advance
+        prefix_xor = 0
+        for num in nums:
+            prefix_xor ^= num
+
+        while q > 0:
+            all_ones = (1 << maximumBit) - 1  #allones of given bitlength will be max number
+            query[index] = prefix_xor ^ all_ones  # xoring allones with current xor will give k
+            num = nums.pop()  # popping last element
+            prefix_xor ^= num     # and removing the contribution of popped element
+            
+            q-= 1
+            index += 1
+            
+        return query 

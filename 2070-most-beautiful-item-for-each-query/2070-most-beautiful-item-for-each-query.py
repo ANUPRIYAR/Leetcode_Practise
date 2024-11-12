f7479c0c2c3fd778db69061fa05d39d1b28c513c
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-12 20:41:04 +0530

    Time: 203 ms (60.98%), Space: 66.5 MB (60.22%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.py
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.py
@@ -1,22 +1,40 @@
 from heapq import heappush , heappop
 class Solution:
     def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
-        maxheap = []
-        answer = [0]* len(queries)
-        items.sort(key = lambda x:(x[0] , x[1]))
-        for i, query in enumerate(queries):
-            for price , beauty in items:
-                if price <= query:
-                    heappush(maxheap, (-beauty , price))
-            if maxheap:
-                b, p = heappop(maxheap)
-                answer[i] = -b
-            # print(max_beauty)
-            # answer.append(max_beauty)
+        
+        bucket = [[0, 0, 9999]]
+        items.sort(key=lambda x:(x[0], x[1]))
+        for price, beauty in items:
+            print(beauty,bucket[-1][1] )
+            if price == bucket[-1][0] and beauty > bucket[-1][1]:
+                bucket[-1][1] = beauty
+            elif beauty > bucket[-1][1]:
+                bucket.append([price, beauty, price])
+        print(bucket)
+
+        answer = []
+        for query in queries:
+            index = self.binary_search(bucket, query)
+            answer.append(bucket[index][1])
 
         return answer
+
             
+                
+    def binary_search(self, bucket, query):
+        left, right = 0, len(bucket) - 1
 
+        while left <= right:
+            mid = left + (right - left)//2
 
-        
-        
+            if bucket[mid][0] < query:
+                lower_index = mid
+                left = mid + 1
+            elif bucket[mid][0] > query:
+                right = mid -1 
+            else:
+                return mid
+
+        return lower_index
+
+       

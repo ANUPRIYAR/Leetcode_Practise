7263ca4a29437f7e9aa0d73c655848d2274064be
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-18 22:16:53 +0530

    Time: 4584 ms (5.03%), Space: 39.7 MB (27.23%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
@@ -1,34 +1,38 @@
 class Solution:
+    def __init__(self):
+        self.directions = [(-1, 1), (0, 1), (1, 1)]
     def maxMoves(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        directions = [(-1, 1), (0, 1), (1, 1)]
+        rows = len(grid)
+        max_moves = 0
+        for row in range(rows):
+            max_moves = max(max_moves, self.dfs(row, 0 ,grid,  0, set()))
+
+        return max_moves
+
+
+    def dfs(self, x, y, grid, moves, visited):
+        visited.add((x, y))
         moves = 0
-
-        dp = [[-1] * cols for _ in range(rows)]
-
-        def dfs(r, c):
-            if dp[r][c] != -1:
-                return dp[r][c]
-
-            max_moves = 0
-
-            for dr, dc in directions:
-                nr, nc = r+ dr, c + dc
-
-                if 0 <= nr < rows and  0 <= nc < cols and grid[nr][nc] > grid[r][c]:
-                    # print(grid[r][c]  ,  grid[nr][nc])
-                    max_moves = max(max_moves, dfs(nr, nc))
-
-            
-            dp[r][c] = max_moves + 1
-            return dp[r][c]
+        for dx, dy in self.directions:
+            if 0<= x + dx < len(grid) and 0<= y + dy < len(grid[0]) and grid[x+dx][y+dy] > grid[x][y] and (x+dx, y+dy) not in visited:
+                moves = max(moves, 1 + self.dfs(x+dx, y +dy, grid, moves, visited))
 
 
-        result = 0
-        for r in range(rows):
-            result = max(result, dfs(r, 0))
+        return moves 
+                
 
-        return result - 1 if result > 0 else result
+         
+
+
+
+
 
         
+
+
+
+
+
+
+
         

0dad776ca489ff2d276bd1817008f9160bc61f7e
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-18 05:59:09 +0530

    Time: 47 ms (11.52%), Space: 17.1 MB (5.59%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -0,0 +1,26 @@
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        new_code = [0]*len(code)
+        n = len(code)
+
+        for i in range(2*len(code)):
+            if k < 0:
+                x = -k 
+            else:
+                x = k
+            for j in range(1, x + 1):
+                print(j)
+                if k > 0:
+                    new_code[i] += code[(i + j)%n]
+                elif k == 0:
+                    new_code[i] = 0
+                else:
+                    # print(i - j%n)
+                    new_code[i] += code[(i - j)%n] 
+
+            if i == len(code) - 1:
+                break
+
+
+        return new_code
+        

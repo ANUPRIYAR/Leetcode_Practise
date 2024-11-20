5b717067191e534c32053a3adda1095b1f7900bc
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-20 20:45:40 +0530

    Time: 287 ms (32.78%), Space: 17.2 MB (79.81%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
@@ -1,30 +1,28 @@
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
-        if k == 0:
-            return 0
+        # if k == 0:
+        #     return 0
 
-        freq = {"a" : 0 , "b" : 0, "c":0}
         n = len(s)
         min_minutes = float('inf')
+        offset = ord("a")
 
-        
         count = [0]*3
         for char in s:
             count[ord(char) - ord("a")] += 1
 
-        for c in count:
-            if c < k:
-                return - 1
+        if min(count) < k:
+            return -1
             
         start = 0
         for end in range(len(s)):
-            freq[s[end]] += 1
+            count[ord(s[end]) - offset] -= 1
 
-            while count[0]- freq["a"] < k or count[1] - freq["b"] < k or count[2] - freq["c"] < k:
-                freq[s[start]] -= 1
+            while min(count) < k:
+                count[ord(s[start]) - offset] += 1
                 start += 1
 
-            if count[0]- freq["a"] >= k and count[1] - freq["b"] >= k and count[2] - freq["c"] >= k:
+            if min(count) >= k:
                 min_minutes = min(min_minutes, n - (end - start + 1))
 
 

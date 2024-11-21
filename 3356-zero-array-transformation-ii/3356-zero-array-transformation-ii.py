be2a7cc2e156c326b71dc2f90594daa21825db11
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-21 23:38:03 +0530

    Time: 159 ms (94.25%), Space: 63.7 MB (5.9%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.py
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.py
@@ -1,29 +1,15 @@
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
         n = len(nums)
-
-        count = [0] * (n + 1)
-        sum_ = 0
-        q_index = 0
-
-
+        cnt = [0 for _ in range(n + 1)]
+        s = k = 0
         for i in range(n):
-            while sum_ + count[i] < nums[i]:
-                # get query
-                q_index += 1
-                print(q_index)
-                if q_index - 1 >= len(queries) :
-                    return -1
-                left, right , val = queries[q_index-1] 
-                count[max(left, i)] += val
-                if right > i and right + 1 < n :
-                    count[right + 1 ] -= val
-
-            sum_ += count[i] 
-        return q_index 
-
-        
-
-        
-
-       
+            while s + cnt[i] < nums[i]:
+                k += 1
+                if k - 1 >= len(queries): return -1
+                l, r, val = queries[k - 1]
+                if r < i: continue
+                cnt[max(l, i)] += val
+                cnt[r + 1] -= val
+            s += cnt[i]
+        return k

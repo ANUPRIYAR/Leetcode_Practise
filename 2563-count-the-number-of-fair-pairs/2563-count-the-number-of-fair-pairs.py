7c101f922c4b39d32fcedae46d1694bf19af1a11
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-24 22:21:06 +0530

    Time: 919 ms (23.63%), Space: 30.6 MB (79.79%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.py
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.py
@@ -1,27 +1,26 @@
 class Solution:
-    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int: 
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         nums.sort()
+
         count = 0
         for i in range(len(nums)):
-            smallest_index = self.find_lower_bound(nums, i +1, len(nums)-1, lower-nums[i])
-            highest_index = self.find_lower_bound(nums, i +1, len(nums)-1, upper + 1-nums[i])
-
-            count += highest_index - smallest_index 
-
+            smallest_index = self.binary_search(nums, i + 1, len(nums) - 1, lower - nums[i])
+            highest_index = self.binary_search(nums, i + 1, len(nums) - 1, upper + 1 - nums[i])
+            count += highest_index - smallest_index
         return count
 
-
-
-    def find_lower_bound(self, arr, left, right, target):
-        
+    
+    def binary_search(self, array, left, right, target):
+         
         while left <= right:
-            mid = (left + right)//2
-            
-            if arr[mid] < target:
+            mid = ( left + right)//2
+
+            if array[mid] < target:
                 left = mid + 1
             else:
-                right = mid - 1
-        
-        return left
+                right  = mid -1 
 
-      
+        return right
+
+        
+

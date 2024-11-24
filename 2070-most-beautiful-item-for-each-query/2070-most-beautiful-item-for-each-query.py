bae64399f84f12061344adeb33efc407faed6ac7
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-24 19:45:33 +0530

    Time: 139 ms (68.1%), Space: 66.4 MB (27.59%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.py
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.py
@@ -1,40 +1,41 @@
-from heapq import heappush , heappop
 class Solution:
     def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
-        
-        bucket = [[0, 0, 9999]]
-        items.sort(key=lambda x:(x[0], x[1]))
+        stack = [(0, 0)]
+        items.sort(key = lambda x:(x[0], x[1]))
         for price, beauty in items:
-            print(beauty,bucket[-1][1] )
-            if price == bucket[-1][0] and beauty > bucket[-1][1]:
-                bucket[-1][1] = beauty
-            elif beauty > bucket[-1][1]:
-                bucket.append([price, beauty, price])
-        print(bucket)
-
-        answer = []
-        for query in queries:
-            index = self.binary_search(bucket, query)
-            answer.append(bucket[index][1])
+            if price == stack[-1][0] and beauty > stack[-1][1]:
+                stack.pop()
+                stack.append((price, beauty))
+            elif beauty > stack[-1][1]:
+                stack.append((price, beauty))
+        print(stack)
+        answer = [0]*len(queries)
+        for i, query in enumerate(queries):
+            beauty = self.binary_search(stack, query)
+            
+            answer[i] = beauty
 
         return answer
-
             
-                
-    def binary_search(self, bucket, query):
-        left, right = 0, len(bucket) - 1
+
+    def binary_search(self, array, target):
+        left = 0
+        right = len(array) - 1
+        lower_value = None
 
         while left <= right:
             mid = left + (right - left)//2
-
-            if bucket[mid][0] < query:
-                lower_index = mid
+            if array[mid][0] < target:
+                lower_value = array[mid][1]
                 left = mid + 1
-            elif bucket[mid][0] > query:
-                right = mid -1 
+            elif array[mid][0] > target:
+                right = mid - 1
             else:
-                return mid
+                return array[mid][1]
 
-        return lower_index
+        return lower_value
 
-       
+
+
+
+        

5f5f3742ff254edfa3c484c15cbaa36aa6558045
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-24 19:09:56 +0530

    Time: 0 ms (100%), Space: 16.8 MB (40.85%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -3,33 +3,34 @@ class MyQueue:
     def __init__(self):
         self.stack1 = []
         self.stack2 = []
+        
 
     def push(self, x: int) -> None:
         self.stack1.append(x)
+
         
+
     def pop(self) -> int:
-        if not self.stack2:
-            while self.stack1:
-                self.stack2.append(self.stack1.pop())
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        popped = self.stack2.pop()
+        self.stack1 = self.stack2[::-1]
+        self.stack2 = []
+        return popped
 
-        # print(self.stack1)
-        return self.stack2.pop()
-        
-        
-
+    
     def peek(self) -> int:
-        if not self.stack2:
-            while self.stack1:
-                self.stack2.append(self.stack1.pop())
-        
-        return self.stack2[-1]
-
-        
-       
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        top_item = self.stack2.pop()
+        self.stack2.append(top_item)
+        self.stack1 = self.stack2[::-1]
+        self.stack2 = []
+        return top_item
         
 
     def empty(self) -> bool:
-        return len(self.stack1) == 0 and len(self.stack2) == 0
+        return len(self.stack1) == 0
         
 
 

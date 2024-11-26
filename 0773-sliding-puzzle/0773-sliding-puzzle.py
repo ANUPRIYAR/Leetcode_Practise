300f29ade78d24273606d97f904feec2fac0d9c4
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-26 08:43:19 +0530

    Time: 7 ms (71.16%), Space: 16.5 MB (96.27%) - LeetHub

--- 0773-sliding-puzzle/0773-sliding-puzzle.py
+++ 0773-sliding-puzzle/0773-sliding-puzzle.py
@@ -1,38 +1,30 @@
 class Solution:
-    def slidingPuzzle(self, grid: List[List[int]]) -> int:
-        target = "123450"
-        # Graph
-        neighbours = {
-            0:[1,3], 1:[0, 2, 4], 2:[1,5],
-            3:[0, 4], 4:[1,3,5], 5:[2,4]
-        }
-
-        start = ''.join([str(num) for row in grid for num in row])
+    def slidingPuzzle(self, board: List[List[int]]) -> int:
+        graph = { 0: [1,3], 1:[0, 2, 4], 2:[1,5],  3:[0,4], 4:[1,3,5] , 5:[2, 4]}
+        
+        target = '123450'
         visited = set()
+        start = ''.join([str(num) for row in board for num in row])
+        queue = deque()
+        queue.append((start, 0))
         visited.add(start)
-
-        queue = deque([(start, 0)])
 
         while queue:
             state, moves = queue.popleft()
 
             if state == target:
                 return moves
-
+            
             zero_index = state.index('0')
 
-            for neigh in neighbours[zero_index]:
+            for child in graph[zero_index]:
                 new_state = list(state)
-                new_state[zero_index], new_state[neigh] = new_state[neigh], new_state[zero_index]
-
+                new_state[zero_index] , new_state[child] = new_state[child], new_state[zero_index]
                 new_state_str = ''.join(new_state)
-                
+
                 if new_state_str not in visited:
                     visited.add(new_state_str)
                     queue.append((new_state_str, moves + 1))
 
         return -1 
-
-
-
-        
+    

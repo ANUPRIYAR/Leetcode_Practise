1d93846c8d5337671c570f8fbb20d4dd6d43cfd7
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-29 19:24:15 +0530

    Time: 4 ms (50.45%), Space: 18 MB (9.68%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -7,20 +7,19 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
 
-        def check_balance_height(node):
+        def find_balanced_height(node):
             if not node:
                 return True, 0
 
-            left_balanced, leftheight = check_balance_height(node.left)
-            right_balanced, rightheight = check_balance_height(node.right)
+            left_balanced, leftheight = find_balanced_height(node.left)
+            right_balanced, rightheight = find_balanced_height(node.right)
 
             current_balanced = left_balanced and right_balanced and abs(leftheight - rightheight) <= 1
-            current_height = max(leftheight, rightheight) + 1
-            return current_balanced , current_height
+            currentheight = max(leftheight, rightheight) + 1
+            return current_balanced, currentheight
 
-        balanced, _ = check_balance_height(root)
+        balanced, _ = find_balanced_height(root)
         return balanced
-            
 
 
         

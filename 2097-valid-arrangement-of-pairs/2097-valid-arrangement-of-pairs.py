06340f9a377aac6bb3ba30ad2f0b6d1797a00af2
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-11-30 07:57:52 +0530

    Time: 330 ms (67.42%), Space: 89.7 MB (57.33%) - LeetHub

---
+++ 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.py
@@ -0,0 +1,31 @@
+class Solution:
+    def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
+        graph = defaultdict(list)
+        inOutDeg = defaultdict(int)
+
+        for start, end in pairs:
+            graph[start].append(end)
+            inOutDeg[start] += 1
+            inOutDeg[end] -= 1
+
+
+        # finding the start node
+        root = pairs[0][0]
+        for node in inOutDeg:
+            if inOutDeg[node] == 1:
+                root = node
+                break
+
+
+        path = []
+        def dfs(curr):
+            while graph[curr]:
+                next_node = graph[curr].pop()
+                dfs(next_node)
+                path.append((curr, next_node))
+
+        dfs(root)
+        print(path)
+        return path[::-1]
+
+

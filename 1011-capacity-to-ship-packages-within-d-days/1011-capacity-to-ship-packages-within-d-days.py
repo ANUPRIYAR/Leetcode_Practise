112a9c6e8bd767862d8a82f97198923abe6bec18
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-12-01 20:52:59 +0530

    Time: 252 ms (22.46%), Space: 21.3 MB (56.73%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -2,8 +2,6 @@ class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
         
         def can_ship(daily_limit):
-            # for weight in weights:
-            #     total_days += math.ceil(weight/x)
             total_weights= 0
             d = 1
             for weight in weights:
@@ -17,12 +15,10 @@ class Solution:
 
         left = max(weights)
         right = sum(weights) * 500
-        result = None
+
         while left <= right:
             mid = left + (right - left)//2
-
             if can_ship(mid):
-                result = mid
                 right = mid -1
             else:
                 left = mid + 1

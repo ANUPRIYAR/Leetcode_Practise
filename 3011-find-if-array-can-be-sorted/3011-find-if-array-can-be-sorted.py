95ee23817742c7da854dce82085411eb50538a49
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-12-01 19:58:04 +0530

    Time: 29 ms (36.98%), Space: 17.2 MB (6.45%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -1,30 +1,27 @@
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
-        n  = len(nums)
-        if n < 2:
-            return True
-        def count_setbits(n):
-            count = 0
-            while n > 0:
-                count += n & 1 # n%2
-                n >>= 1  # n//2
-            return count
-
         start = 0
-        for end in range(1,n):
-            if count_setbits(nums[end - 1]) != count_setbits(nums[end]):
-                max_element = max(nums[start:end])
-                print(f"maxelement : {max_element}")
+        for end in range(1, len(nums)):
+            if self.count_setbits(nums[end]) != self.count_setbits(nums[end - 1]):
+                previous_max = max(nums[start:end])
                 j = end
-                while j < n-1 and count_setbits(nums[j]) == count_setbits(nums[j + 1]):
+                while j < len(nums)-1 and self.count_setbits(nums[j]) == self.count_setbits(nums[j+1]):
                     j += 1
-                min_element = min(nums[end:j +1 ])
-                print(f"minelement : {min_element}")
-                if max_element > min_element:
+                
+                currentmin = min(nums[end: j+1])
+                if previous_max > currentmin:
                     return False
 
                 start = end
+               
 
         return True
-                
-                 
+
+    def count_setbits(self, num):
+        count = 0
+        while num > 0:
+            count += num & 1
+            num >>= 1
+
+        return count
+        

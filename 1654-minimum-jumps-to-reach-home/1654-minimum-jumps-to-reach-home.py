05b9968bdbc5f823e9adfcd571d9071a09571a1b
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-12-06 19:37:56 +0530

    Time: 170 ms (15.22%), Space: 88 MB (5.02%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
@@ -1,39 +1,14 @@
 class Solution:
     def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
-        forbidden_set = set(forbidden)
-        max_position = max(x + a + b, 2000)  # Set an upper limit for exploration
-        memo = {}
+        F = set(forbidden)
         visited = set()
-
-        def dfs(position, jumps, last_jump):
-            # Base case: if we've reached home
-            if position == x:
-                return jumps
-
-            visited.add((position, last_jump))
-            
-            # If out of bounds or forbidden
-            if position < 0 or position > max_position or position in forbidden_set and (position, last_jump) not in visited:
-                return float('inf')
-            
-            # Memoization check
-            if (position, last_jump) in memo:
-                return memo[(position, last_jump)]
-            
-            # Explore forward jump
-            forward_jumps = dfs(position + a, jumps + 1, 'F')
-            
-            # Explore backward jump only if last was not backward
-            backward_jumps = float('inf')
-            if last_jump != 'B':
-                backward_jumps = dfs(position - b, jumps + 1, 'B')
-
-            # Store result in memoization dictionary
-            result = min(forward_jumps, backward_jumps)
-            memo[(position,last_jump)] = result
-            
-            return result
-
-        res = dfs(0, 0,'F')  # Start from position 0 with 0 jumps and no previous direction
-        
-        return res if res != float('inf') else -1
+        @lru_cache(None)
+        def dfs(i,isPrevBack):
+            if (i,isPrevBack) in visited: return math.inf
+            else: visited.add((i,isPrevBack))
+            if i == x: return 0
+            if i in F or i > 6000: return math.inf
+            if isPrevBack or i - b <= 0: return 1 + dfs(i + a, False)
+            return 1 + min(dfs(i + a, False), dfs(i - b, True))
+        ans = dfs(0,False)
+        return ans if ans < math.inf else -1

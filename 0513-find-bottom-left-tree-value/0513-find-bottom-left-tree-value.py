beebd40674432660ce8a4f367d86a04ea792aa94
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-12-06 09:39:30 +0530

    Time: 3 ms (55.17%), Space: 19.4 MB (40%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -19,21 +19,27 @@ class Solution:
             for i in range(len(queue)):
                 node = queue.popleft()
 
+                if i == 0:
+                    leftmostnode = node.val
+                    
+
                 if node.left:
                     queue.append(node.left)
-                    prev_node = node
+                    
+                    # prev_node = node
                 
                 if node.right:
                     queue.append(node.right)
-                    if not node.left:
-                        prev_node = node
+                    # if not node.left:
+                        # prev_node = node
 
-        if prev_node.left:
-            return prev_node.left.val
-        elif prev_node.right:
-            return prev_node.right.val
 
-        return prev_node.val
+        # if prev_node.left:
+        #     return prev_node.left.val
+        # elif prev_node.right:
+        #     return prev_node.right.val
+
+        return leftmostnode
 
         
 

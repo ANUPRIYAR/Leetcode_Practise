897e745462c65c3fd854f631d84c14f4ea385a6e
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-12-07 11:41:26 +0530

    Time: 78 ms (54.99%), Space: 17.6 MB (53.77%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
@@ -1,14 +1,18 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
-        offset = ord('a')
+        char_map = dict()
+        for ord_i in range(ord('a'), ord('z')):
+            char_map[chr(ord_i)] = chr(ord_i + 1)
+        char_map['z'] = 'a'
+        print(char_map)
+
         i, j = 0, 0
-
-        while i < len(str1)  and j < len(str2):
-
-            if str1[i] == str2[j] or (ord(str1[i]) - offset + 1)% 26 == ord(str2[j]) - offset:
+        while i < len(str1) and j < len(str2):
+            if str1[i] == str2[j] or char_map[str1[i]] == str2[j]:
                 i += 1
                 j += 1
             else:
                 i += 1
 
         return j == len(str2)
+        

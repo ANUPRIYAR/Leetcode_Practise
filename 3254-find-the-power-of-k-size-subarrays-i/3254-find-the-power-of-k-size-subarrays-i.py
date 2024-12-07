85d2b6811a92e23e49aef169661702fdeb1e9553
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-12-07 09:36:52 +0530

    Time: 41 ms (33.22%), Space: 17.7 MB (5.29%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
@@ -1,28 +1,33 @@
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
-        def is_consecutive_and_sorted(subarray):
-            return all(subarray[i] + 1 == subarray[i +1] for i in range(len(subarray) - 1))
-            
+        answer = [-1]* (len(nums) - k + 1)
 
-        n = len(nums)
-        result = []
-
-        for i in  range(n - k + 1):
-            subarray = nums[i:i+ k]
-            if is_consecutive_and_sorted(subarray):
-                result.append(max(subarray))
-            else:
-                result.append(-1)
-
-        return result
-
-    
-                
-                
-           
-
-
-            
-
+        if len(nums) == 1 and k == 1:
+            return nums
         
+        if k == 1:
+            return nums
+
+        consecutive = 1
+        index = 0
+        for end in range(1, len(nums)):
+            print(f"end  : {end} consicutive : {consecutive}")
+            if nums[end] -1 == nums[end - 1]:
+                consecutive += 1
+                print(f"end  : {end} consicutive : {consecutive}")
+            else:
+                consecutive = 1
+
+            if consecutive == k:
+                answer[end - k + 1] = nums[end]
+                consecutive -= 1
+                print(f"index : {end - k + 1} nums[end] : {nums[end]}")
+
+            
+
+        return answer
+
+
+
+
         

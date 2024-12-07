71120f2c35ff164361a24674eebf248f21f9f665
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-12-07 08:02:42 +0530

    Time: 91 ms (83.58%), Space: 36.8 MB (8.15%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,27 +1,32 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        begin = 0
-        freq = dict()
-        window_sum = 0
-        max_sum = 0
+        start = 0
+        max_sum = - float('inf')
+        
+        num_freq = dict()
+        for num  in nums[:k]:
+            num_freq[num] = num_freq.get(num, 0) + 1
 
-        for end in range(len(nums)):
-            freq[nums[end]] = freq.get(nums[end], 0) + 1
-            window_sum += nums[end]
+        window_sum = sum(nums[:k])
+        if len(num_freq) == k:
+            max_sum = max(max_sum, window_sum)
+        
 
-            while freq[nums[end]] > 1 or end - begin + 1 > k:
-                freq[nums[begin] ] -= 1
-                window_sum -= nums[begin]
-                begin += 1
+        for end in range(len(nums) - k ):
+            window_sum -= nums[end]
+            window_sum += nums[end + k]
 
-            if end - begin + 1 == k:
+            # Remove first num from window
+            num_freq[nums[end]] -= 1
+            if num_freq[nums[end]] == 0 : del num_freq[nums[end]]
+            # Add current num
+            num_freq[nums[end + k]] = num_freq.get(nums[end + k], 0) + 1
+
+            if len(num_freq) == k:
                 max_sum = max(max_sum, window_sum)
 
-
-        return max_sum
-            
-
+        return max_sum if max_sum != -float('inf') else 0
+ 
 
 
-
-
+        

5340d7cf5dbe9803bab889e15eb7f8f50c7dc44f
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-12-07 16:34:06 +0530

    Time: 0 ms (100%), Space: 17.4 MB (9.46%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,49 +1,41 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         adj = defaultdict(list)
-
-        for i,( u, v) in enumerate(equations) :
+        for i, (u, v) in enumerate(equations):
             adj[u].append((v, values[i]))
             adj[v].append((u, 1/values[i]))
+        
 
-
-        def compute_graph(a, b):
+        def bfs(src, dest):
             visited = set()
-
             queue = deque()
-            queue.append((a, 1))
+            queue.append((src, 1))
 
             while queue:
                 node, cur_val = queue.popleft()
 
                 if node in visited:
                     continue
-
                 visited.add(node)
 
                 for nei, val in adj[node]:
-                    if nei == b:
+                    if nei == dest:
                         return cur_val * val
                     else:
                         queue.append((nei, cur_val*val))
 
             return -1 
 
-        answers = []
+        answer = []
         for u, v in queries:
             if u not in adj or v not in adj:
-                answers.append(-1) 
-            elif u == v:
-                answers.append(1)
+                answer.append(-1)
+            elif u == v :
+                answer.append(1)
             else:
-                answers.append(compute_graph(u, v))
+                answer.append(bfs(u, v))
 
-        return answers
-                    
-                    
-
-
-        
+        return answer
 
 
         

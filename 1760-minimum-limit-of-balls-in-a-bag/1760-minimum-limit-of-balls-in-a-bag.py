5ac098636f05a198f1feeb9b6f106b9f0932fb6b
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-12-07 19:54:51 +0530

    Time: 815 ms (35.26%), Space: 29.4 MB (26.36%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.py
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.py
@@ -1,30 +1,21 @@
 class Solution:
     def minimumSize(self, nums: List[int], maxOperations: int) -> int:
         def helper(bagsize):
-            idealbagcount = (sum(nums)//bagsize)
-            operations = idealbagcount - len(nums)
-            # operations = 0
-            # for item_count in nums:
-            #     if item_count > bagsize:
-            #         operations += 1
-            #         cursize = item_count - idealbagcount
-            #         while cursize 
-            #         nums.append(item_count - idealbagcount)
+            operations = 0
+            for ballcount in nums:
+                operations += math.ceil(ballcount/bagsize) - 1
+                if operations > maxOperations:
+                    return False
 
-            return operations <= maxOperations
+            return True
 
         left = 1
-        right = max(nums) 
-        while left < right:
+        right = max(nums)
+
+        while left <= right:
             mid = left + (right - left)//2
-            ans = None
             if helper(mid):
-                ans = mid
-                right = mid 
+                right = mid - 1
             else:
                 left = mid + 1
-
-        return right
-                    
-
-        
+        return left

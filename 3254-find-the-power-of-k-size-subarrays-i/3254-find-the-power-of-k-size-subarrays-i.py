797f83798d0d57af1c979567365ab24e3be8556e
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-12-07 09:37:56 +0530

    Time: 0 ms (100%), Space: 17.6 MB (5.29%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
@@ -9,21 +9,16 @@ class Solution:
             return nums
 
         consecutive = 1
-        index = 0
+
         for end in range(1, len(nums)):
-            print(f"end  : {end} consicutive : {consecutive}")
             if nums[end] -1 == nums[end - 1]:
                 consecutive += 1
-                print(f"end  : {end} consicutive : {consecutive}")
             else:
                 consecutive = 1
 
             if consecutive == k:
                 answer[end - k + 1] = nums[end]
                 consecutive -= 1
-                print(f"index : {end - k + 1} nums[end] : {nums[end]}")
-
-            
 
         return answer
 

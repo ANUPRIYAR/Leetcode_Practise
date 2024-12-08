f04e0b58d9eeb37ad596108c2b94345f0f69e4d6
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-12-08 18:41:11 +0530

    Time: 0 ms (100%), Space: 17.4 MB (7.66%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+       
+        def helper(n , memo):
+            if n in memo:
+                return memo[n]
+            if n==0:
+                return 1
+
+            if n < 0:
+                return 0
+
+            memo[n] =  helper(n-1, memo) + helper(n-2, memo) 
+            return memo[n]
+
+        return helper(n, {})
+        
+        

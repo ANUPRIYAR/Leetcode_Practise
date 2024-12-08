54753b1b33a55ec7434d450df67f57b82db9db28
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-12-08 23:44:48 +0530

    Time: 158 ms (72.82%), Space: 57.3 MB (65.25%) - LeetHub

---
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        events.sort(key = lambda x: x[1])
+
+        max_value = 0
+        result = 0
+
+        max_until = []
+        end_times = []
+
+        for start, end, value in events:
+            index = bisect.bisect_left(end_times, start) - 1
+            max_previous = max_until[index] if index >= 0 else 0
+
+            result = max(result, value + max_previous)
+
+            max_value = max(max_value, value)
+            max_until.append(max_value)
+            end_times.append(end)
+
+        return result

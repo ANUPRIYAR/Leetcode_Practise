7b549a252d204e23148b6d0951925cec6147a3bc
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-12-10 00:30:11 +0530

    Time: 5 ms (69.37%), Space: 17.6 MB (5.55%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
@@ -1,28 +1,23 @@
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
         answer = [-1]* (len(nums) - k + 1)
-
-        if len(nums) == 1 and k == 1:
-            return nums
-        
+        consecutive = 1
         if k == 1:
             return nums
-
-        consecutive = 1
-
-        for end in range(1, len(nums)):
-            if nums[end] -1 == nums[end - 1]:
+        
+        for end in range(1,len(nums)):
+            if nums[end] - 1 == nums[end -1]:
                 consecutive += 1
             else:
                 consecutive = 1
-
+            # print(f"consecutive:{consecutive} end:{end}")
             if consecutive == k:
                 answer[end - k + 1] = nums[end]
+                # print(f"end - k + 1 : {end - k + 1}")
                 consecutive -= 1
 
         return answer
+            
 
-
-
-
+        
         
